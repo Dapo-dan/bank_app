d1ddec175ad7dd190f49a0efec7ab3887f0ae227
Oladapo Daniel OLATUBOSUN <odapo138@gmail.com>
2023-12-17 23:03:16 +0100

    fixed mistake in import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import {useState} from 'react'
 
 import { close, logo, menu } from '../assets'
-import { navlinks } from '../constants'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'></nav>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      Test
+    </nav>
   )
 }
 
